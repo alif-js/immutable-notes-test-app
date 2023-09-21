15360c72a778888bca491ff76013f70d3e6a3a18
alif-js <alif.hubspot@gmail.com>
2023-09-21 20:58:51 +0600

    added remove functionality

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ document.getElementById('btn-post').addEventListener('click',function(){
 
     newComment.value = '';
 })
+
+document.getElementById('btn-remove').addEventListener('click',function(){
+    // const newComment = document.getElementById('new-comment');
+    // const commentContent = newComment.value;
+
+    const commentContainer = document.getElementById('comment-container');
+    // const p = document.createElement('p');
+    // p.innerText = commentContent;
+
+    commentContainer.removeChild(commentContainer.lastChild);
+
+    newComment.value = '';
+})
